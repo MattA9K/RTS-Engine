d781ed871b2e41911e51357eb5ee9bfd419ac2b3
Mateusz Andrzejczuk <CiniCraft@Mateuszs-iMac.local>
2016-07-14 12:34:31 -0700

    isolated maps from generator logic.

---
+++ 3D RPG Engine/🗺Map01.swift
@@ -0,0 +1,16 @@
+//
+//  Map01.swift
+//  3D RPG Engine
+//
+//  Created by Mateusz Andrzejczuk on 7/14/16.
+//  Copyright © 2016 Core Prime Inc. All rights reserved.
+//
+
+import Foundation
+
+
+extension GameMap {
+    
+    
+
+}
